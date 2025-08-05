53974f654e861a01713a70bdb42ba75241e20df8
ISABELLE DOS SANTOS DE ABREU <25132976@CFP127ALUNO.LOCAL>
2025-08-05 16:57:21 -0300

    projeto em andamento

---
+++ app.js
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * Objetivo: Desenvolver uma aplicação para a empresa viva moda
+ * Autor: Isabelle
+ * Data: 05/08/2025
+ * Versão: 1.0
+ ******************************************************************************/
+//Import da biblioteca
+//const nome = require('./cliente.js')
+
+//Função de entrada de dados
